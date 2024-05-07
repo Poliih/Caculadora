1987271d443a6930abebe38872dd1c919aa2572f
Poliana Rodrigues <104945360+Poliih@users.noreply.github.com>
2024-05-07 00:01:01 -0300

    Primeiro Comit

---
+++ Calculadora/src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;   
+    height: 75px; 
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+
+    input {
+        width: 100%;
+        height: 75px; 
+        background-color: #AAAAFF;
+        border: 0;
+        font-size: 24px;
+        font-family: 'Roboto';
+        display: flex;
+        padding: 0 10px;
+        flex-direction: column;
+        align-items: flex-end;
+        color: #ffffff;
+        
+    }
+    
+`;
